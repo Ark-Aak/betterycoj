abfe2d7124ede100bb5a0af095f8c873132a45ae
Aak <111547592+Ark-Aak@users.noreply.github.com>
2024-04-18 21:58:09 +0800

    Update betterycoj.user.js

--- betterycoj.user.js
+++ betterycoj.user.js
@@ -35,7 +35,7 @@ const stdId = "iws3c1kp";
 let solutionMapping = [];
 let standardMapping = [];
 const colorMap = ["#7F7F7F", "#FE4C61", "#F39C11", "#FFC116", "#52C41A", "#3498DB", "#9D3DCF", "#0E1D69", "#000000"];
-const diffMap = ["暂无评定", "入门", "普及−", "普及/提高−", "普及+/提高", "提高+/省选−", "省选/NOI−", "NOI/NOI+/CTSC", "NOI++/CTSC+"];
+const diffMap = ["暂无评定", "入门", "普及−", "普及/提高−", "普及+/提高", "提高+/省选−", "省选/NOI−", "NOI/NOI+/CTSC", "<font color=\"red\">NOI++/CTSC+</font>"];
 const version = "1.2.4";
 const code300 = "#include <bits/stdc++.h>\nint main(){while(clock()*1.0/CLOCKS_PER_SEC<0.8){}int a,b;std::cin>>a>>b;std::cout<<a+b;}";
 let uid, clientId, csrf;
